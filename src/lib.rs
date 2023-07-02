bcee81e05b45ae1c81842af8aa6a6c9a05ebd4c8
Daisuke SHIGETA <shigepon7@gmail.com>
2023-07-03 08:14:31 +0900

    for test

--- src/lib.rs
+++ src/lib.rs
@@ -66,36 +66,34 @@
 //!   let mut client = Client::default();
 //!   client.set_timeout(5);
 //!   client.connect_ws()?;
-//!   loop {
-//!     for (filter, event) in client.next_event_filtered_all()?.iter() {
-//!       let Some(commit) = event.as_commit() else {
-//!         continue;
-//!       };
-//!       let posts = commit.get_post_text();
-//!       if posts.is_empty() {
-//!         continue;
-//!       }
-//!       let text = posts.join(" ").replace("\n", " ");
-//!       let time = commit.time.with_timezone(&Local).format("%m/%d %H:%M");
-//!       let handle = match client.get_repo(&commit.repo) {
-//!         Ok(r) => r.handle.clone(),
-//!         _ => String::from("UNKNOWN"),
-//!       };
-//!       let blobs = commit
-//!         .blobs
-//!         .iter()
-//!         .map(|b| b.to_string())
-//!         .collect::<Vec<_>>();
-//!       print!("{} : {} : {} : {}", filter, time, handle, text);
-//!       if !commit.blobs.is_empty() {
-//!         println!(" : {}", blobs.join(","));
-//!       } else {
-//!         println!("");
-//!       }
-//!       stdout().flush().ok();
+//!   for (filter, event) in client.next_event_filtered_all()?.iter() {
+//!     let Some(commit) = event.as_commit() else {
+//!       continue;
+//!     };
+//!     let posts = commit.get_post_text();
+//!     if posts.is_empty() {
+//!       continue;
 //!     }
-//!     std::thread::sleep(Duration::from_millis(10));
+//!     let text = posts.join(" ").replace("\n", " ");
+//!     let time = commit.time.with_timezone(&Local).format("%m/%d %H:%M");
+//!     let handle = match client.get_repo(&commit.repo) {
+//!       Ok(r) => r.handle.clone(),
+//!       _ => String::from("UNKNOWN"),
+//!     };
+//!     let blobs = commit
+//!       .blobs
+//!       .iter()
+//!       .map(|b| b.to_string())
+//!       .collect::<Vec<_>>();
+//!     print!("{} : {} : {} : {}", filter, time, handle, text);
+//!     if !commit.blobs.is_empty() {
+//!       println!(" : {}", blobs.join(","));
+//!     } else {
+//!       println!("");
+//!     }
+//!     stdout().flush().ok();
 //!   }
+//!   Ok(())
 //! }
 //! ```
 
